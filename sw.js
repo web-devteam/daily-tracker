8290ea382bf39877aaa4a48f55bc67a4aa96446e
emmanuel597 <139566832+emmanuel597@users.noreply.github.com>
2025-01-05 14:22:09 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,19 @@
-const CACHE_NAME = 'offline-cache';
-const OFFLINE_URL = '/daily-tracker/offline.html'; // Update the path based on your repo name
+const cacheName = 'offline-page';
+const cachedFiles = ['/daily-tracker/','/daily-tracker/index.html','/daily-tracker/index.css','/daily-tracker/index.js','/daily-tracker/1432527.png','/daily-tracker/256px-Spotify_icon.svg.png','/daily-tracker/pngwing.com (1).png','/daily-tracker/Youtube Music Icon (PNG480p) - Vector69Com.png','/daily-tracker/more_vert_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.png','/daily-tracker/add_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.png','/daily-tracker/bar_chart_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.png','/daily-tracker/calendar_month_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.png','/daily-tracker/chevron_left_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.png','/daily-tracker/chevron_right_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.png'];
 
 self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll([OFFLINE_URL]);
+        caches.open(cacheName).then(cache => {
+            console.log('Opened cache');
+            return cache.addAll(cachedFiles);
         })
     );
 });
 
 self.addEventListener('fetch', event => {
-    if (event.request.mode === 'navigate') {
-        event.respondWith(
-            fetch(event.request).catch(() => {
-                return caches.open(CACHE_NAME).then(cache => {
-                    return cache.match(OFFLINE_URL);
-                });
-            })
-        );
-    }
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
 });
