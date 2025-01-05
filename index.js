bbd4188bb31891eee1b1621dfbedb19622a47733
emmanuel597 <139566832+emmanuel597@users.noreply.github.com>
2025-01-05 04:39:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -789,3 +789,13 @@ console.log(wkstart)
 console.log(wkend)
 }
 }}
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js')
+    .then(function(registration) {
+        console.log('Service Worker registered with scope:', registration.scope);
+    }).catch(function(error) {
+        console.log('Service Worker registration failed:', error);
+    });
+}
+
