1de0b4807e34e9925eb561e34545984d08434a78
emmanuel597 <139566832+emmanuel597@users.noreply.github.com>
2025-01-09 20:16:21 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dailytrackerpages';
+const CACHE_NAME = 'dailytrackerpagesv1';
 const urlsToCache = ['/daily-tracker/','/daily-tracker/index.html','/daily-tracker/index.css','/daily-tracker/index.js','/daily-tracker/1432527.png','/daily-tracker/256px-Spotify_icon.svg.png','/daily-tracker/pngwing.com (1).png','/daily-tracker/Youtube Music Icon (PNG480p) - Vector69Com.png','/daily-tracker/more_vert_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.png','/daily-tracker/add_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.png','/daily-tracker/bar_chart_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.png','/daily-tracker/calendar_month_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.png','/daily-tracker/chevron_left_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.png','/daily-tracker/chevron_right_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.png'];
 
 self.addEventListener('install', event => {
